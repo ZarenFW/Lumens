32c364895ef41b1b06b190cb7b679540d898b12b
ZarenFW <joseph.pp.gore@gmail.com>
2020-07-24 16:00:01 +0100

    Adding Dusts

---
+++ src/main/java/com/Zaren/Lumens/items/ItemDust.java
@@ -0,0 +1,12 @@
+package com.Zaren.Lumens.items;
+
+import com.Zaren.Lumens.Lumens;
+import net.minecraft.item.Item;
+
+public class ItemDust extends Item {
+
+    public ItemDust() {
+
+        super(new Properties().group(Lumens.TAB));
+    }
+}
